e17b53b01ebb3b374c248f0ae3ee1b2ece9a82ac
praveen28624 <68008375+praveen28624@users.noreply.github.com>
2022-06-22 12:44:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
-<?php exec("sudo apt install ffmpeg");
-exec("ffmpeg -i https://lightning-traceurban-samsungau.amagi.tv/playlist.m3u8 a.mp3");
+<?php shell_exec("apt install ffmpeg -y");
+shell_exec("ffmpeg -i https://lightning-traceurban-samsungau.amagi.tv/playlist.m3u8 a.mp3");
 include_once("index.html"); ?>
